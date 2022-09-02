44344e26adc938cee72c9d1937f2d18eb52001ca
MNRezende <miguelrezende91@gmail.com>
2022-09-02 08:12:57 -0300

    falhando a aplicacao

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,6 +2,6 @@
 describe('home page', ()=> {
     it('app deve estar online', ()=> {
         cy.visit('/')
-        cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+        cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eatss')
     })
 })
